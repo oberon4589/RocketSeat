e1030ac82cbe45c8ac654e2def5e290fa6cd1a85
SKINKA <oberon4589@gmail.com>
2023-10-11 14:01:24 -0300

    feat: criando o banco de dados

--- src/main/java/br/com/skinkalabz/todolist/user/UserModel.java
+++ src/main/java/br/com/skinkalabz/todolist/user/UserModel.java
@@ -1,12 +1,28 @@
 package br.com.skinkalabz.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users") // tb_users é o nome da tabela no banco de dados
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID") // O Spring JPA fica
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
