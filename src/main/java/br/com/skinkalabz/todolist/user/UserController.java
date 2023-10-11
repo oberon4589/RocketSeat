6757249c47bef04b631b9b18e88feafc23777323
SKINKA <oberon4589@gmail.com>
2023-10-11 14:14:08 -0300

    feat: adicionando usuários ao banco de dados

--- src/main/java/br/com/skinkalabz/todolist/user/UserController.java
+++ src/main/java/br/com/skinkalabz/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.skinkalabz.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,24 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (número inteiro)
-     * Double (número decimal)
-     * Boolean (verdadeiro ou falso)
-     * Float (número decimal)
-     * Char (caractere)
-     * Date (data)
-     * Void (vazio)
-     */
-
-    /**
-     * Body precisa ser passado pois é o corpo da requisição
-     */
+    @Autowired
+    private IUserRepository userRepository;
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 
 }
