571cfc2da39bf58b8f943688aafe16a537917f36
SKINKA <oberon4589@gmail.com>
2023-10-13 16:54:12 -0300

    feat: verificando se senha e usuário são validos

--- src/main/java/br/com/skinkalabz/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/skinkalabz/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.skinkalabz.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.skinkalabz.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -30,11 +36,19 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String[] credentials = authString.split(":"); // Divide a string em duas partes, separadas pelo ":"
         String username = credentials[0];
         String password = credentials[1];
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
 
         // Verificar se o usuário e senha estão corretos
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+        }
         // Se estiverem corretos, deixa a requisição passar
 
         filterChain.doFilter(request, response);
