6be7f1f975dff27112475e543e891cbdca7b2672
SKINKA <oberon4589@gmail.com>
2023-10-13 15:01:43 -0300

    feat: criptografando a senha do usuário

--- src/main/java/br/com/skinkalabz/todolist/user/UserController.java
+++ src/main/java/br/com/skinkalabz/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificador
  * public: significa que a classe UserController pode ser acessada por qualquer
@@ -32,6 +34,11 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray()); // Função que criptografa a senha
+
+        userModel.setPassword(passwordHashred); // Atribui a senha criptografada ao usuário
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
