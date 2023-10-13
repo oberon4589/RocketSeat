13654241a06b5fbeaa756e201048e6007c8192f0
SKINKA <oberon4589@gmail.com>
2023-10-13 16:38:35 -0300

    feat: autenticando o usuário

--- src/main/java/br/com/skinkalabz/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/skinkalabz/todolist/filter/FilterTaskAuth.java
@@ -1,27 +1,43 @@
 package br.com.skinkalabz.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-        // Executar alguma ação
+        // Pegar a autenticação (usuário e senha)
+        var authorization = request.getHeader("Authorization");
 
-        System.out.println("Chegou no filtro!");
-        chain.doFilter(request, response);
+        var authEncoded = authorization.substring("Basic".length()).trim(); // Remove o "Basic" do início da string
 
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded); // Decodifica a string
+
+        var authString = new String(authDecode); // Transforma o array de bytes em string
+
+        String[] credentials = authString.split(":"); // Divide a string em duas partes, separadas pelo ":"
+        String username = credentials[0];
+        String password = credentials[1];
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+
+        // Verificar se o usuário e senha estão corretos
+        // Se estiverem corretos, deixa a requisição passar
+
+        filterChain.doFilter(request, response);
     }
 
 }
