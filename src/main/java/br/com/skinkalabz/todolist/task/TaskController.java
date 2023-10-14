fb4ef673ca2ee2b5a9f63bf2fdd0a3300f12a511
SKINKA <oberon4589@gmail.com>
2023-10-14 09:52:15 -0300

    feat: validando usuário para alterar tarefas

--- src/main/java/br/com/skinkalabz/todolist/task/TaskController.java
+++ src/main/java/br/com/skinkalabz/todolist/task/TaskController.java
@@ -54,12 +54,24 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
 
         var task = this.taskRepository.findById(id).orElse(null);
+        // Valida se a task existe
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada!");
+        }
+
+        // Valida se a task pertece ao usuário logado
+        var idUser = request.getAttribute("idUser");
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Você não tem permissão para alterar essa tarefa!");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
